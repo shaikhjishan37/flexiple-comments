5761b52e9f110d954ef1f8384e504bdd04468292
jshaikh <jshaikh@digite.com>
2023-05-08 20:08:15 +0530

    added build files

---
+++ build/precache-manifest.362c88f55e65f7bc46027b6c22dc5fc9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440493ec9d9b47f5eef6c9fb96228152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d8386b54d2b70d6e7d",
+    "url": "/static/css/main.751010d4.chunk.css"
+  },
+  {
+    "revision": "b1efab63cae323c8378e",
+    "url": "/static/js/2.79a13f6a.chunk.js"
+  },
+  {
+    "revision": "9349a01fb576665dd8ebe0a9e4b9f4af",
+    "url": "/static/js/2.79a13f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d8386b54d2b70d6e7d",
+    "url": "/static/js/main.9e5e9cb2.chunk.js"
+  },
+  {
+    "revision": "3715f300ab3d2f83827e",
+    "url": "/static/js/runtime-main.15219c79.js"
+  },
+  {
+    "revision": "2d15decc87393ec398f00dc3d7a2163d",
+    "url": "/static/media/Lato-Bold.2d15decc.ttf"
+  },
+  {
+    "revision": "0f73589acfddad17f91be2f7b6e284a0",
+    "url": "/static/media/Lato-Light.0f73589a.ttf"
+  },
+  {
+    "revision": "c5eb54404c9dbda925f7142d6343c913",
+    "url": "/static/media/Lato-Regular.c5eb5440.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "4b42ae9763502e652a262a1e94ec7596",
+    "url": "/static/media/icomoon.4b42ae97.ttf"
+  },
+  {
+    "revision": "7bfaa5dd0907b7c9a188c129b7c2b349",
+    "url": "/static/media/icomoon.7bfaa5dd.svg"
+  },
+  {
+    "revision": "b254d36240e3a556dffdf1b80377d06a",
+    "url": "/static/media/icomoon.b254d362.woff"
+  }
+]);
