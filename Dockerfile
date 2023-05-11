b89d8191bb2ffbc9441871faa16aff99e227cd5a
jshaikh <jshaikh@digite.com>
2023-05-11 11:55:14 +0530

    added build for latest changes in app

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,10 @@ VOLUME /tmp
 RUN mkdir /app
 WORKDIR /app
 
-RUN npm install
-RUN yarn build
 RUN yarn add express cors 
 RUN yarn add elastic-apm-node
 
+COPY /build /app
 COPY server.js /app
 COPY entrypoint.sh /app
 
---
+++ build/precache-manifest.7183fb55f870ec785421a7b692d79787.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26aedad70e9204e143987fd367ce231e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab79abaaa1063e62c2b2",
+    "url": "/static/css/main.b032dcb3.chunk.css"
+  },
+  {
+    "revision": "4a8c0a431ec924c1a1a0",
+    "url": "/static/js/2.e1a7cbb3.chunk.js"
+  },
+  {
+    "revision": "9349a01fb576665dd8ebe0a9e4b9f4af",
+    "url": "/static/js/2.e1a7cbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab79abaaa1063e62c2b2",
+    "url": "/static/js/main.4259266c.chunk.js"
+  },
+  {
+    "revision": "3715f300ab3d2f83827e",
+    "url": "/static/js/runtime-main.15219c79.js"
+  },
+  {
+    "revision": "2d15decc87393ec398f00dc3d7a2163d",
+    "url": "/static/media/Lato-Bold.2d15decc.ttf"
+  },
+  {
+    "revision": "0f73589acfddad17f91be2f7b6e284a0",
+    "url": "/static/media/Lato-Light.0f73589a.ttf"
+  },
+  {
+    "revision": "c5eb54404c9dbda925f7142d6343c913",
+    "url": "/static/media/Lato-Regular.c5eb5440.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "4b42ae9763502e652a262a1e94ec7596",
+    "url": "/static/media/icomoon.4b42ae97.ttf"
+  },
+  {
+    "revision": "7bfaa5dd0907b7c9a188c129b7c2b349",
+    "url": "/static/media/icomoon.7bfaa5dd.svg"
+  },
+  {
+    "revision": "b254d36240e3a556dffdf1b80377d06a",
+    "url": "/static/media/icomoon.b254d362.woff"
+  }
+]);
